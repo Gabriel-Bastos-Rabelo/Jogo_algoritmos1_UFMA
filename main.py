0e13028af799669aae59f0380bfedac477247212
Gabriel <gbastosrabelo@gmail.com>
2022-12-17 16:43:24 -0300

    atualização3

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ import time
 
 
 pygame.init()
-#aqui definimos a largura e a altura que vai ter a tela do jogo
+# Definição da música do jogo e seu volume
+pygame.mixer.music.set_volume(0.17)
+musica_de_fundo = pygame.mixer.music.load("musica_fundo.ogg")
+pygame.mixer.music.play(-1)
+#musica_da_derrtota = pygame.mixer.music.load('/home/GabrielBastos/Área de trabalho/Jogo_versao2_ Algoritmos1_ufma/jogo-/smw_game_over.wav')
+
+
+# aqui definimos a largura e a altura que vai ter a tela do jogo
 largura = 975
 altura = 720
 
@@ -16,15 +23,17 @@ pygame.display.set_caption("Mata Mosquito")
 posicaoX = random.randint(0, 880)
 posicaoY = random.randint(0, 600)
 # Esse será o som que irá ser disparado a cada clique do mouse no sprite
-songbotton = pygame.mixer.Sound("smw_shell_ricochet.wav")
-#Essas são as definições da fonte que iremos usar para escrever os textos na tela
+songbotton = pygame.mixer.Sound("songbottom.wav")
+# Essas são as definições da fonte que iremos usar para escrever os textos na tela
 fonte = pygame.font.SysFont("arial", 40, True, False)
 
 # Função para retornar uma tupla com dois valores randômicos, um para X e outro para Y
+
+
 def posicaoRandomica():
     posicaoX = random.randint(0, 880)
     posicaoY = random.randint(0, 600)
-    return (posicaoX,posicaoY)
+    return (posicaoX, posicaoY)
 
 
 class mosca(pygame.sprite.Sprite):
@@ -47,14 +56,14 @@ class mosca(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, (610//7, 511//7))
         self.rect = self.image.get_rect()
         self.rect.topleft = nova_posicao
-        
 
-nova_posicao =posicaoRandomica()
+
+nova_posicao = posicaoRandomica()
 sprites = pygame.sprite.Group()
 mosca = mosca()
 sprites.add(mosca)
 # Plano de fundo da tela e seu tamanho
-background_image = pygame.image.load("park_background.webp").convert()
+background_image = pygame.image.load("background.webp").convert()
 background_image = pygame.transform.scale(background_image, (largura, altura))
 # definições iniciais do jogo
 pontos = 0
@@ -63,24 +72,25 @@ vidas = 5
 while True:
     #  Usando a forma de armazenar dados em arquivo, implementamos o highscore do jogo,
     #  onde para cada iteração é verificado se a pontuação do jogador é maior que o highscore
-    with open("highscore.txt","r") as highscore:
+    with open("highscore.txt", "r") as highscore:
         for pontuação in highscore.readlines():
             highscore = pontuação
-            if pontos>int(pontuação):
-                with open("highscore.txt","w") as maior_pontuação:
+            if pontos > int(pontuação):
+                with open("highscore.txt", "w") as maior_pontuação:
                     maior_pontuação.write(str(pontos))
     # textos que irão aparecer na tela
     mensagem_highscore = f"Highscore: {highscore}"
     mensagem_pontos = f"Pontos: {pontos}"
     mensagem_vidas = f"Vidas: {vidas}"
-    texto_pontos_formatado = fonte.render(mensagem_pontos, False, (0,0,0))
-    texto_highscore_formatado = fonte.render(mensagem_highscore, False, (0,0,0))
-    texto_vidas_formatado = fonte.render(mensagem_vidas, False, (0,0,0))
+    texto_pontos_formatado = fonte.render(mensagem_pontos, False, (0, 0, 0))
+    texto_highscore_formatado = fonte.render(
+        mensagem_highscore, False, (0, 0, 0))
+    texto_vidas_formatado = fonte.render(mensagem_vidas, False, (0, 0, 0))
     # pega o tempo em milisecundos a cada iteração e transforma para segundos
     tempo_inicial = pygame.time.get_ticks()/1000
 
     # verifica se as vidas do jogador acabaram, caso tenha acabado, o jogo termina
-    if vidas <= 0:
+    if vidas == 0:
         print("Você perdeu")
         exit()
     # verifica se o intervalo entre o ultimo clique feito pelo jogador e o tempo corrido do momento é maior que 1.2 segundos,
@@ -111,8 +121,8 @@ while True:
                 nova_posicao = posicaoRandomica()
                 sprites.update()
                 ultimo_tempo = pygame.time.get_ticks()/1000
-                
-    #posicionamento do plano de fundo
+
+    # posicionamento do plano de fundo
     tela.blit(background_image, (0, 0))
     sprites.draw(tela)
     tela.blit(texto_pontos_formatado, (737, 36))
